39b83b483c76ecfbb75cef073e71e521117f9b1b
Rezwan Arefin <rezwanarefin01@gmail.com>
2024-01-11 00:26:28 +0600

    Gamma correct image

--- lib/color.ml
+++ lib/color.ml
@@ -1,10 +1,10 @@
 open Base
-
 include Vec3
 
 let to_string t =
+  let gamma_correct v = Float.sqrt v in
   let quantize v =
-    let v = Float.clamp_exn v ~min:0.0 ~max:1.0 in
+    let v = Float.clamp_exn (gamma_correct v) ~min:0.0 ~max:1.0 in
     Int.of_float Float.(v * 255.0)
   in
   Printf.sprintf "%d %d %d" (quantize t.x) (quantize t.y) (quantize t.z)
